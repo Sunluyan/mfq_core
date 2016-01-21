fc8f13d82fe4e6b02cb835a7e555b2deeffc151e
liuzhiguo <410915218@qq.com>
2016-01-21 14:17:28 +0800

    提交

--- src/main/java/com/mfq/bean/OperationRecord.java
+++ src/main/java/com/mfq/bean/OperationRecord.java
@@ -1,8 +1,12 @@
 package com.mfq.bean;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class OperationRecord {
+public class OperationRecord implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
     private Long id;
 
     private Long uid;
