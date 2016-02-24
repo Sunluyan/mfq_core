847f2aa99137d12e39b1d6e6fdc8d522c02fbc79
liuzhiguo <410915218@qq.com>
2016-02-24 18:33:58 +0800

    提交`

---
+++ src/main/java/com/mfq/bean/SysOperationRecord.java
@@ -0,0 +1,85 @@
+package com.mfq.bean;
+
+import java.util.Date;
+
+public class SysOperationRecord {
+    private Long id;
+
+    private Integer sysId;
+
+    private Long userId;
+
+    private Date time;
+
+    private Integer type;
+
+    private String content;
+
+    private String username;
+
+    private String sysUsername;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getSysId() {
+        return sysId;
+    }
+
+    public void setSysId(Integer sysId) {
+        this.sysId = sysId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getSysUsername() {
+        return sysUsername;
+    }
+
+    public void setSysUsername(String sysUsername) {
+        this.sysUsername = sysUsername == null ? null : sysUsername.trim();
+    }
+}
