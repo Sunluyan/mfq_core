a1878a3d71f3df025b8cae6447a6038ee78f0bba
hui <326191813@qq.com>
2016-04-01 18:57:18 +0800

    eee

--- src/test/java/com/mfq/test/TestController.java
+++ src/test/java/com/mfq/test/TestController.java
@@ -2,16 +2,14 @@ package com.mfq.test;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.mfq.bean.user.Gender;
 import com.mfq.bean.user.User;
 import com.mfq.helper.SignHelper;
@@ -247,8 +245,13 @@ public class TestController {
 //        findBiggest("daaaabbbbcccccccc");
 //        testProductDetail();
 
-        testUserDetail();
+//        testUserDetail();
+
+
+        HashSet<Integer> u = Sets.newHashSet();
+
     }
+
 
 
 }
